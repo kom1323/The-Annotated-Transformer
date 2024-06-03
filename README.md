eb4621d3aa532c4bc7aa84406031dfe3a376a6f6
kom1323 <omer2313@gmail.com>
2024-06-03 13:11:39 +0300

    Finished basic encoder. Next is Decoder

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # The-Annotated-Transformer
 
-This repository follows Harvard's "The Annotated Transformer" blog.
+This repository follows Harvard's "The Annotated Transformer" blog. 
 Check it out at https://nlp.seas.harvard.edu/annotated-transformer/
---
+++ encoder_layer.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+from sublayer_connection import SublayerConnection
+from encoder import clones
+
+class EncoderLayer(nn.Module):
+    "Encoder is made up of self-attn and feed forward (defined below)"
+
+    def __init__(self, size, self_attn, feed_forward, dropout) -> None:
+        super(EncoderLayer, self).__init__()
+        self.self_attn = self_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 2)
+        self.size = size
+
+
+    def forward(self, x, mask):
+        "Follow transformer figure (left) fro connections."
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
+        return self.sublayer[1](x, self.feed_forward)
